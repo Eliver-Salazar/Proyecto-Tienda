66c17cbceb5277dd6aa99a014b1da560061ab233
Eliver-Salazar <eliverslzrcmps@gmail.com>
2025-11-25 18:45:39 -0600

    Semana 10 y 11
    Commit

---
+++ src/main/java/proyectotienda/repository/RolRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package proyectotienda.repository;
+
+import proyectotienda.domain.Rol;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+    public Optional<Rol> findByRol(String rol);
+
+}
